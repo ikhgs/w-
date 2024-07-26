4e657bbea7f4057b05a98b6c18f581b4aa7dbf0e
ikhgs <mathematiques45@gmail.com>
2024-07-26 19:20:20 +0300

    Update twilio_api.py

--- helper/twilio_api.py
+++ helper/twilio_api.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+account_sid = os.getenv('ACb1e27aa70ecc3659544b58a8a3450afa')
+auth_token = os.getenv('Tb8064608c257774ee16e0f230d0d81fd')
 client = Client(account_sid, auth_token)
 
 
