6f17e6af23ddb59e68ca3ea1a4df7ea5e01d85d1
ikhgs <mathematiques45@gmail.com>
2024-07-26 21:01:47 +0300

    Update openai_api.py

--- helper/openai_api.py
+++ helper/openai_api.py
@@ -2,8 +2,7 @@ import os
 
 
 import openai
-from dotenv import load_dotenv
-load_dotenv()
+
 
 OPENAI_API_KEY ='sk-proj-dku33dCFA4CRNwVUkjRsT3BlbkFJfZWrgeJenYTzNSiNo2Xn'
 openai.api_key = os.getenv('OPENAI_API_KEY')
